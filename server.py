aed401c3fabb08fe7f4d677ababc49f096ca2aaf
Theia Ware <theia.j.ware@gmail.com>
2022-01-22 22:12:08 -0700

    Make the API pull from DB
    My implementation is different than the one in the tutorial.
    Mostly I wanted to do things more 'cleanly' or at least try
    to excercise my own knowledge of list and dictionary comprehension.

--- server.py
+++ server.py
@@ -1,44 +1,54 @@
 from flask import Flask, request, jsonify
+import sqlite3
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'year_published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samual R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'year_published': '1975'}
-]
+def dictionaryRowFactory(cursor, row):
+    return { column[0]: row[i] for i, column in enumerate(cursor.description) }
 
+def fetchAllBooks(query, params={}):
+    conn = sqlite3.connect('books.db')
+    conn.row_factory = dictionaryRowFactory
+    return conn.cursor().execute(query, params).fetchall()
 
 @app.route('/', methods = ['GET'])
 def home():
-    return '<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>'
+    return '''<h1>Distant Reading Archive</h1>
+<p>This site is a prototype API for distant reading of science fiction novels.</p>'''
 
+@app.errorhandler(404)
+def pageNotFound(e):
+    return '<h1>404</h1><p>The resource could not be found.</p>', 404
+
+### Book APIs
 @app.route('/api/v1/resources/books/all', methods = ['GET'])
-def api_all():
+def apiBooksAll():
+    books = fetchAllBooks('SELECT * FROM books;')
     return jsonify(books)
 
 @app.route('/api/v1/resources/books', methods = ['GET'])
-def api_id():
-    # Check if an id was provided
-    if 'id' in request.args:
-        id = int(request.args['id'])
-    else:
-        return 'Error: No id field provided. Please specify an id.'
-    
-    return jsonify([ b for b in books if b['id'] == id ])
+def apiBooksFilter():
+    # Fields we want to filter on
+    fields = [ 'id', 'published', 'author' ] 
+
+    # Make filter dictionary based on which request args
+    # are sent
+    queryFilter = {
+        f: request.args[f]
+        for f in fields
+        if request.args.get(f)
+    }
+
+    # Build up our where clause
+    whereClause = ' AND '.join([
+        '{0}=:{0}'.format(f)
+        for f in queryFilter.keys()
+    ])
+
+    query = "SELECT * FROM books WHERE {};".format(whereClause)
+    books = fetchAllBooks(query, queryFilter)
+    return jsonify(books)
 
 app.run()
 
